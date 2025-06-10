58e243c208d59c6e054fe25d15de954e66716b33
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-10 22:30:50 +0300

    L-337. Implement fetch cart data functionality in new cart-actions.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Notification from './components/UI/Notification.js';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { sendCartData } from './store/cart-slice.js';
+import { sendCartData, fetchCartData } from './store/cart-actions.js';
 
 let isInitial = true; // we use it so that useEffect cannot send data when opening the App for the first time
 
@@ -15,6 +15,10 @@ function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
   const notification = useSelector(state => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   /**
    * We use Google FireBase database.
