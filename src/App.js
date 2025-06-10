de5d3f7ef333d54b257bfb69e4bb21174510e6f9
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-10 21:05:50 +0300

    L-333. Save/update cart data to the database whenever we add/remove an item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  /**
+   * We use Google FireBase database.
+   * Asynchronous functionality should NOT get executed in a reducer. 
+   * It can be executed in this useEffect with cart as a selector and useEffect dependency.
+   * This way, whenever the store changes the cart, fetch will update the database.
+   */
+  useEffect(() => {
+    fetch('https://redux-cart-ed864-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+      {
+        method: 'PUT', // overwites the entirely json data
+        body: JSON.stringify(cart)
+      }
+    );
+  }, [cart]);
 
   return (
     <Layout>
