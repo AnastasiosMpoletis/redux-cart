25f449c19332506d2e3e11b77f43b7f38e6635f0
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-06-10 21:54:46 +0300

    L-336. Move and refactor sendCartData functionality to cart-slice.js, by creating a custom action

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Notification from './components/UI/Notification.js';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { uiActions } from './store/ui-slice.js';
+import { sendCartData } from './store/cart-slice.js';
 
 let isInitial = true; // we use it so that useEffect cannot send data when opening the App for the first time
 
@@ -23,45 +23,12 @@ function App() {
    * This way, whenever the store changes the cart, fetch will update the database.
    */
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }));
-
-      const response = await fetch('https://redux-cart-ed864-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT', // overwites the entire json data
-          body: JSON.stringify(cart)
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      // const responseData = await response.json();
-
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!',
-      }));
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch(error => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!',
-      }));
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { uiActions } from "./ui-slice.js";
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -38,6 +39,47 @@ const cartSlice = createSlice({
         }
     }
 });
+
+export const sendCartData = (cart) => {
+    return async (dispatch) => {
+        dispatch(uiActions.showNotification({
+            status: 'pending',
+            title: 'Sending...',
+            message: 'Sending cart data!'
+        }));
+
+        const sendRequest = async () => {
+            const response = await fetch('https://redux-cart-ed864-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+                {
+                    method: 'PUT', // overwites the entire json data
+                    body: JSON.stringify(cart)
+                }
+            );
+
+            if (!response.ok) {
+                throw new Error('Sending cart data failed.');
+            }
+
+            // const responseData = await response.json();
+        }
+
+        try {
+            await sendRequest();
+
+            dispatch(uiActions.showNotification({
+                status: 'success',
+                title: 'Success!',
+                message: 'Sent cart data successfully!',
+            }));
+        } catch (error) {
+            dispatch(uiActions.showNotification({
+                status: 'error',
+                title: 'Error!',
+                message: 'Sending cart data failed!',
+            }));
+        }
+    }
+};
 
 export const cartActions = cartSlice.actions;
 
